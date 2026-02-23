d0ecdcdbb720fe382c3780cc401ace9efd724045
Pedro Marques <pdro.marqs2015@hotmail.com>
2026-02-23 00:03:09 -0300

    feat: setup + desenhando game objects em tela

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#include "iostream"
+#include "raylib.h"
+
+int main()
+{
+    std::cout << "Starting the game " << "\n";
+
+    const int SCREEN_WIDTH = 1280;
+    const int SCREEN_HEIGHT = 800;
+    const char* TITLE = "Pong Game";
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE);
+
+    SetTargetFPS(75);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+
+        // Drawing game objects
+        DrawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, WHITE);
+        DrawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20, WHITE);
+        DrawRectangle(10, SCREEN_HEIGHT / 2 - 60, 25, 120, WHITE);
+        DrawRectangle(SCREEN_WIDTH - 35, SCREEN_HEIGHT / 2 - 60, 25, 120, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
